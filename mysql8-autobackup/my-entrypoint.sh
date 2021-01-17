37fa3f92a2df7ee76aaeacf2b094ef910cb6c3c7
李栋 <cnlidong@live.cn>
2021-01-17 11:15:53 +0800

    更新使用docker-cron v1.0.2版本

--- mysql8-autobackup/my-entrypoint.sh
+++ mysql8-autobackup/my-entrypoint.sh
@@ -4,7 +4,7 @@ echo "开始启动docker-cron"
 echo $*
 cd /docker-cron/
 env
-./docker-cron-linux-amd64-v1.0.1 &
+./docker-cron-linux-amd64-v1.0.2 &
 cd -
 echo "docker-cron启动完成,继续执行原始启动命令!"
 docker-entrypoint.sh $*
